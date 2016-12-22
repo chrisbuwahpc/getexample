01f005f6925cb80c8d919a603c2a289b2f1916e5
Rangah <17440279@student.curtin.edu.au>
2016-12-22 12:10:36 +0800

    hybrid file

---
+++ magnus_examples/hello_world/hybrid_ranga/hybrid_hello.f90
@@ -0,0 +1,29 @@
+PROGRAM HYBRID
+IMPLICIT NONE
+include 'mpif.h'
+
+!iopenMPI variables
+ integer :: rank
+ integer :: size
+ integer :: ierror
+ integer :: tag 
+ integer :: status (MPI_STATUS_SIZE)
+!openMP variables
+     INTEGER :: NTHREADS, TID, OMP_GET_NUM_THREADS
+
+!local variables
+ integer :: i
+
+call MPI_INIT(ierror)
+call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierror)
+call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
+
+DO i=0, size -1
+
+ IF (rank == i) THEN
+   print*, 'node', rank, ': Hello world'
+ END IF
+END DO
+
+call MPI_FINALIZE(ierror)
+end PROGRAM HYBRID
