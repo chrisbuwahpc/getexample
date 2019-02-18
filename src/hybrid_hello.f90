54bbd258b2722bd159de223891d3515108417397
Chris.Bording <Chris.Bording@ibm.com>
2019-02-18 16:40:34 +0000

    update fortranHybrid_gnu and f90 source

--- src/hybrid_hello.f90
+++ src/hybrid_hello.f90
@@ -6,18 +6,21 @@ PROGRAM HYBRID_HELLO
   INCLUDE 'mpif.h'
   
   ! OpenMPI variables:
-  INTEGER :: rank, numprocs
+  INTEGER :: rank, numtasks
   INTEGER :: ierror 
   INTEGER, dimension(MPI_STATUS_SIZE) :: status
-  
+  INTEGER :: len
+  CHARACTER*(MPI_MAX_PROCESSOR_NAME) :: name
+ 
   ! OpenMP variables:
   INTEGER :: TID=0 
   INTEGER :: NTHREADS=1
   
   CALL MPI_INIT(ierror)
-  CALL MPI_COMM_SIZE(MPI_COMM_WORLD, numprocs, ierror)
+  CALL MPI_COMM_SIZE(MPI_COMM_WORLD, numtasks, ierror)
   CALL MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
   
+  CALL MPI_GET_PROCESSOR_NAME(name, len, ierror)
   
   !$OMP PARALLEL PRIVATE(TID,NTHREADS)
   ! Obtain thread number
@@ -26,7 +29,7 @@ PROGRAM HYBRID_HELLO
   ! Obtain total number of threads
   NTHREADS = OMP_GET_NUM_THREADS()
 
-  PRINT *, 'Hello World from thread=', TID, 'out of', NTHREADS, 'from processes', rank
+  PRINT *, 'Hello World from thread=', TID, 'out of', NTHREADS, 'from mpi rank', rank, 'hostname',name
   !$OMP END PARALLEL
   
   CALL MPI_FINALIZE(ierror)
